85273e5ec777f1ca745124fc517202d23be7757f
Pimeq <rzepecki2@gmail.com>
2024-01-29 12:40:56 +0100

    zmiana na mnożęnie w metodzie policz

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int Policz(int a, int b)
+{
+    return a * b;
+}
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "pierwszy branch 2*3=: " << Policz(3, 4) << std::endl;
+    return 0;
+}
