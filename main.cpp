0eddcb187600dfc13d8ffaf7f67a132cecc3734e
Hubert Rzepecki <54513008+Pimeq@users.noreply.github.com>
2024-01-29 13:05:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int Policz(int a, int b)
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "pierwszy branch 2=3=: " << Policz(2, 3) << std::endl;
+    std::cout << "pierwszy branch 2*3=: " << Policz(2, 3) << std::endl;
     return 0;
 }
